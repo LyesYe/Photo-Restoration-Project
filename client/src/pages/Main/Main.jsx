ce59a529e8936416285297ad0aff1ae308cd0fe6
Lyes Khoumeri <64961951+LyesYe@users.noreply.github.com>
2023-01-28 00:22:31 +0100

    push

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -9,48 +9,25 @@ function Main() {
 const handleUploadImage = async(ev) => {
     ev.preventDefault();
 
-    // console.log(img_obj)
-
     const filou = new FormData();
-    // data.append('filename', img_obj['filename']);
     filou.append('file', img_obj);
 
     try {
 
       console.log(img_obj)
 
-
-      for (var pair of filou.entries()) {
-        console.log(pair[0]+ ', ' + pair[1]); 
-    }
-  
-      // const response = await axios({
-      //   method: "POST",
-      //   url:"http://127.0.0.1:5000/upload",
-      //   headers: { "content-type": file.type },
-      //   data: {
-      //     data: img_obj,
-      //   },
-        
-      // })
       const response = await axios({
         method: "POST",
         url:"http://127.0.0.1:5000/upload",
         headers: { "content-type": "multipart/form-data" },
-        data: {
-          data: filou,
-        },
+        data: filou,
+        
         
       })
-
-
-
     }
     catch (err) {
       console.log(err);
     }
-
-
     
   }
 
